8a1295dc2e2673bcdf19ea759673dd951d6a92ec
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-13 11:26:06 -0300

    Feat: Header create

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,18 @@
+
+import { useRouter } from 'next/router';
+import Link from "next/link";
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  const router = useRouter();
+
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Link href={'/'}>
+        <img src="/Logo.svg" alt="logo"/>
+        </Link>
+      </div>
+    </header>
+  );
 }
