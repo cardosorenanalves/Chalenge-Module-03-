d18903de71ced5ea9970995813e33b1fe8a25298
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-14 10:37:34 -0300

    Feat: home page functional

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 import { FiUser, FiCalendar } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
@@ -30,23 +34,47 @@ interface HomeProps {
 }
 
 export default function Home({ postsPagination }: HomeProps) {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [nextPage, setNextPage] = useState<string>('');
+
+  useEffect(() => {
+    setPosts(postsPagination.results);
+    setNextPage(postsPagination.next_page);
+  }, []);
+
+  function getOtherPosts() {
+    fetch(nextPage)
+      .then(t => t.json())
+      .then(response => {
+        const data = [...posts, ...response.results];
+        setNextPage(response.next_page);
+        setPosts(data);
+      });
+  }
+
   return (
     <main className={styles.conteiner}>
       <div className={styles.post}>
-        {postsPagination.results.map(post => (
-          <a key={post.uid} href="#">
-            <strong>{post.data.title}</strong>
-            <p className={styles.content}>{post.data.subtitle}</p>
-            <div className={styles.info}>
-              <FiCalendar />
-              <time>{post.first_publication_date}</time>
-              <FiUser />
-              <p>{post.data.author}</p>
-            </div>
-          </a>
+        {posts.map(post => (
+          <Link href={`/post/${post.uid}`}>
+            <a key={post.uid} href="#">
+              <strong>{post.data.title}</strong>
+              <p className={styles.content}>{post.data.subtitle}</p>
+              <div className={styles.info}>
+                <FiCalendar />
+                <time>{post.first_publication_date}</time>
+                <FiUser />
+                <p>{post.data.author}</p>
+              </div>
+            </a>
+          </Link>
         ))}
       </div>
-      {postsPagination.next_page != null ? <h1>olaaaaaaa</h1> : null}
+      {nextPage != null ? (
+        <div className={styles.loadingPosts} onClick={getOtherPosts}>
+          <p>Carregar mais posts</p>
+        </div>
+      ) : null}
     </main>
   );
 }
@@ -56,10 +84,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
   const response = await prismic.getByType('posts', {
     lang: 'pt-BR',
-    pageSize: 2,
   });
-
-  console.log(response);
 
   const results = response.results.map(post => {
     return {
