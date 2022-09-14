2e35c3e311986576d1d386f12022727c04c4672e
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-14 12:12:51 -0300

    Feat: getStaticProps and getStaticPaths create

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,7 @@
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,20 +29,80 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post() {
+  return (
+    <main>
+      {/* <img src="" alt="banner do post" />
+      <article className={styles.post}>
+        <h1>{post.title}</h1>
+        <div className={styles.info}>
+          <FiCalendar />
+          <time>{post.first_publication_date}</time>
+          <FiUser />
+          <p>{post.data.author}</p>
+        </div>
+        <div
+          className={styles.postContent}
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
+      </article> */}
+    </main>
+  );
+}
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
+export const getStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.getByType('posts', {
+    pageSize: 1,
+    lang: 'pt-BR',
+  });
 
-//   // TODO
-// };
+  const response = posts.results.map(post => {
+    return { params: { slug: post.uid } };
+  });
 
-// export const getStaticProps = async ({params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
+  return {
+    paths: response,
+    fallback: true,
+  };
+};
 
-//   // TODO
-// };
+export const getStaticProps = async ({ params }) => {
+  const prismic = getPrismicClient();
+
+  const { slug } = params;
+
+  const response = await prismic.getByUID('posts', String(slug), {});
+
+  console.log(response);
+
+  const post = {
+    first_publication_date: format(
+      new Date(response.last_publication_date),
+      'dd MMM yyyy',
+      {
+        locale: ptBR,
+      }
+    ),
+    data: {
+      title: Array.isArray(response.data.title)
+        ? RichText.asText(response.data.title)
+        : response.data.title,
+      subtitle: response.data.subtitle,
+      author: response.data.author,
+      banner: response.data.banner,
+      content: response.data.content.map((value: any) => {
+        return {
+          heading: value.heading,
+          body: value.body,
+        };
+      }),
+    },
+  };
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
